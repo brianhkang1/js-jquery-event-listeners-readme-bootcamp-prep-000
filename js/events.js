d47f3d6d51cf2511c5c84715639ec46bba1d735c
brianhkang1@gmail.com <brianhkang1@gmail.com>
2018-06-19 04:29:34 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -21,14 +21,11 @@ function pressIt() {
 
 function submitIt(){
   $("form").on("submit", function() {
-  if ($("input:first").val() === "correct") {
-    alert('Your form is going to be submitted now.');
+  alert('Your form is going to be submitted now.');
     return;
-  }
-  alert("You entered the wrong value");
-  return;
-});
+  })
 }
+
 
 $(document).ready(function(){
 getIt();
